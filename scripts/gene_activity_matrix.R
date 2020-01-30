de7e25c6dbe7033f158ef54cc39e9743a95578cd
caramirezal <caramirezal@ciencias.unam.mx>
2020-01-30 15:25:03 +0100

    calculation of gene expression matrix

--- scripts/gene_activity_matrix.R
+++ scripts/gene_activity_matrix.R
@@ -5,7 +5,7 @@ library(Matrix)
 ## barcodes
 barcodes <- read.table('../data/satpathy2016/barcodes.tsv', 
                        header = TRUE)  
-barcodes.s <- barcodes$Barcodes
+barcodes.s <- as.character(barcodes$Barcodes)
 summaryTCells <- readRDS('../data/satpathy2016/scATAC_TME_TCells_SummarizedExperiment.final.rds')
 
 ## peaks
@@ -15,14 +15,21 @@ peaks <- as.character(peaks$Feature)
 ## counts
 counts <- readMM('../data/satpathy2016/matrix.mtx')
 rownames(counts) <- as.character(peaks)
-colnames(counts) <- barcodes.s
+colnames(counts) <- as.character(barcodes.s)
 
 ## create seurat object
 counts_seu <- CreateSeuratObject(counts = counts, project = 'tcells', min.cells = 1, min.features = 1)
+remove(counts)
+counts_seu <- NormalizeData(counts_seu)
+counts_seu <- FindVariableFeatures(counts_seu, 
+                                     selection.method = 'vst',
+                                     nfeatures = 10000)
+var_features <- VariableFeatures(counts_seu)
+counts_fs <- counts_seu@assays$RNA@counts[var_features, ]
 
 ## generate gene activity matrix
 activity.matrix <- CreateGeneActivityMatrix(
-        peak.matrix = GetAssayData(counts_seu, slot='counts'), 
+        peak.matrix = counts_fs, 
         annotation.file = "../data/Homo_sapiens.GRCh38.98.gtf.gz", 
         seq.levels = c(1:22, "X", "Y"), 
         upstream = 2000, 
