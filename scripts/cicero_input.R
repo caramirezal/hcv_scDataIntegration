40356c9922b792688e23c597c8de696e366c5353
caramirezal <caramirezal@ciencias.unam.mx>
2020-02-06 17:48:53 +0100

    cicero pipe

--- scripts/cicero_input.R
+++ scripts/cicero_input.R
@@ -2,3 +2,13 @@
 library(cicero)
 
 input_cds <- make_atac_cds('../data/counts_satpathy.rds', binarize = TRUE)
+
+set.seed(33)
+input_cds <- detectGenes(input_cds)
+input_cds <- estimateSizeFactors(input_cds)
+input_cds <- preprocessCDS(input_cds, norm_method = "none")
+input_cds <- reduceDimension(input_cds, max_components = 2, num_dim=6,
+                             reduction_method = 'tSNE', norm_method = "none")
+tsne_coords <- t(reducedDimA(input_cds))
+row.names(tsne_coords) <- row.names(pData(input_cds))
+cicero_cds <- make_cicero_cds(input_cds, reduced_coordinates = tsne_coords)
