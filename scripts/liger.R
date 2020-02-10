8e272c7d2f4cf6c911da3618f658752078ea2090
caramirezal <caramirezal@ciencias.unam.mx>
2020-02-10 11:45:42 +0100

    adding function to run standard liger pipeline

--- scripts/liger.R
+++ scripts/liger.R
@@ -3,35 +3,59 @@
 library(liger)
 library(dplyr)
 
-## read Miller rna data 
-rna <- readRDS('~/sc/singleCellDeconvolution/data/cell_lines_rna.RDS')
-
-## Read Satpathy atac data
-atac <- readRDS('~/sc/singleCellDeconvolution/data/cell_lines_activity_matrix.RDS')
-colnames(atac) <- paste0(colnames(atac), '.atac')
-
-## preprocessing data
-liger <- createLiger(list(rna=rna, atac=atac))
-liger <- normalize(liger)
-liger <- selectGenes(liger, combine = 'union')
-liger <- scaleNotCenter(liger)
 
 
-## running liger
-liger <- optimizeALS(liger, k = 4) 
-liger <- quantileAlignSNF(liger) #SNF clustering and quantile alignment
+## gene expression + atac integration
+run_liger <- function(rna_path,
+                      gam_path,
+                      k=10,
+                      liger_path,
+                      plot_path,
+                      interspecies=NULL){
+        cat('Reading rna dataset\n')
+        rna <- readRDS(rna_path)
+        
+        cat('Readin gene activity matrix from ATAC seq\n')
+        atac <- readRDS(gam_path)
+        colnames(atac) <- paste0(colnames(atac), '.atac')
+        
+        cat('Preprocessing data\n')
+        liger <- createLiger(list(rna=rna, atac=atac))
+        liger <- normalize(liger)
+        liger <- selectGenes(liger, combine = 'union', capitalize=interspecies)
+        liger <- scaleNotCenter(liger)
+        
+        cat('Running liger\n')
+        liger <- optimizeALS(liger, k = k) 
+        liger <- quantileAlignSNF(liger) 
+        
+        cat('Saving results\n')
+        saveRDS(liger, liger_path)
+        
+        cat('Run TSNE\n')
+        ligerex = runTSNE(ligerex)
+        
+        cat('Plotting visualizations\n')
+        pdf(plot_path)
+        plotByDatasetAndCluster(ligerex) #Can also pass in different set of cluster labels to plot
+        plotFeature(ligerex, "nUMI")
+        plotWordClouds(ligerex)
+        plotGeneLoadings(ligerex)
+        dev.off()
+}
 
-## saving results
-saveRDS(liger, 'data/cell_lines_liger.RDS')
 
 
-ligerex = runTSNE(ligerex)
-# for larger datasets, may want to use UMAP instead
-ligerex = runUMAP(ligerex)
 
-pdf("liger_results_lcmv.pdf")
-plotByDatasetAndCluster(ligerex) #Can also pass in different set of cluster labels to plot
-plotFeature(ligerex, "nUMI")
-plotWordClouds(ligerex)
-plotGeneLoadings(ligerex)
-dev.off()
+run_liger(
+        rna_path = '../data/rna.rds',
+        gam_path = '../data/gene_activity_matrix_cd8tcells.rds',
+        k = 10,
+        liger_path = '../data/liger_cd8tcells.rds',
+        plot_path = '../figures/liger_cd8tcells.pdf',  
+        interspecies = TRUE
+)
+
+
+
+
