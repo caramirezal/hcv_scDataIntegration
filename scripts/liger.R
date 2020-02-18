7ea274b9a266a23c57fdecb0bde0567917834e78
caramirezal <cramireza@ciencias.unam.mx>
2020-02-18 17:40:25 +0100

    liger run

--- scripts/liger.R
+++ scripts/liger.R
@@ -47,15 +47,61 @@ run_liger <- function(rna_path,
 
 
 
-run_liger(
-        rna_path = '../data/rna_counts_miller.rds',
-        gam_path = '../data/gene_activity_matrix_satpathy_cd8.rds',
-        k = 10,
-        liger_path = '../data/liger_cd8tcells.rds',
-        plot_path = '../figures/liger_cd8tcells.pdf',  
-        interspecies = TRUE
-)
+## setting parameters and paths
+#rna_path = '../data/rna_counts_miller.rds'
+#gam_path = '../data/gene_activity_matrix_satpathy_cd8.rds'
+#k = 10
+#liger_path = '../liger_cd8tcells.rds'
+#plot_path = '../liger_cd8tcells.pdf'  
+#interspecies = TRUE
+
+#cat('reading rna counts')
+#rna <- readRDS(rna_path)
+        
+#cat('Readin gene activity matrix from ATAC seq\n')
+#atac <- readRDS(gam_path)
+#colnames(atac) <- paste0(colnames(atac), '.atac')
+        
+#cat('Preprocessing data\n')
+#liger <- createLiger(list(rna=rna, atac=atac))
+#liger <- normalize(liger)
+#liger <- selectGenes(liger, combine = 'union', capitalize=interspecies)
+#liger <- scaleNotCenter(liger)
+        
+#cat('Running liger\n')
+#liger <- optimizeALS(liger, k = k) 
+#liger <- quantileAlignSNF(liger) 
+        
+        
+#cat('Run TSNE\n')
+#liger = runTSNE(liger)
+
+#cat('Saving results to ', liger_path, '\n')
+#saveRDS(liger, liger_path)
+        
+#cat('Plotting visualizations to ', plot_path, '\n')
+#pdf(plot_path)
+#plotByDatasetAndCluster(liger) #Can also pass in different set of cluster labels to plot
+#plotFeature(liger, "nUMI")
+#plotWordClouds(liger)
+#plotGeneLoadings(liger)
+#dev.off()
+
+liger_save <- function(liger,
+                       path_dest = '.'
+             ){
+      res <- list(
+                  H_norm = liger@H.norm,
+                  H = liger@H,
+                  tsne = liger@tsne.coords,
+                  W = liger@W,
+                  V = liger@V,
+                  cluster = liger@clusters
+             )
+       cat('Saving results \n')
+       saveRDS(res, path_dest)
+}
 
 
-
-
+#saveRDS(res, '../data/liger_cd8tcells_tsne.rds')
+#liger_in <- readRDS('../data/TEx_batch_correction.RDS')
